03b886e0f4fe91da44e7a1080dc546c0d6f8d95b
mylhyz <mylhyz@gmail.com>
2024-05-04 23:02:26 +0800

    引入markdown组件和copy组件

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
-import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Markdown from "react-markdown";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function App() {
+  const markdownS = "# Hi, *Pluto*!";
   return (
     <div className="App">
-      <Button variant="contained">Hello world</Button>
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+      <CopyToClipboard text="Hello!">
+        <button>Copy to clipboard</button>
+      </CopyToClipboard>
+      <Markdown>{markdownS}</Markdown>
     </div>
   );
 }
