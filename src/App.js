a25000e165f60fba013c951a15a3df990a694a91
mylhyz <mylhyz@gmail.com>
2024-05-07 09:00:37 +0800

    访问本地文件

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 import Markdown from "react-markdown";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
@@ -9,13 +13,28 @@ function App() {
   const [year, setYear] = useState("2024");
   const [user, setUser] = useState("mylhyz");
   const [licenceShortStr, setLicenseShortStr] = useState("");
+  const [age, setAge] = useState("");
   const [licenceShortMarkdownStr, setLicenseShortMarkdownStr] = useState("");
   useEffect(() => {
+    fetch(process.env.PUBLIC_URL + "/resources/licenses.json")
+      .then((res) => res.json())
+      .then(
+        (res) => {
+          console.log(res);
+        },
+        (err) => {
+          console.error(err);
+        }
+      );
     setLicenseShortStr("");
   }, []);
   useEffect(() => {
     setLicenseShortMarkdownStr("# Hi, *Pluto*!");
   }, [licenceShortStr]);
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
   return (
     <div className="App">
       <TextField
@@ -36,6 +55,23 @@ function App() {
           setUser(event.target.value);
         }}
       />
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-helper-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-helper-label"
+          id="demo-simple-select-helper"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
       <CopyToClipboard text="Hello!">
         <Button variant="contained">拷贝到剪贴板</Button>
       </CopyToClipboard>
